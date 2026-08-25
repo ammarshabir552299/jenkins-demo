9ff5bb3188ec8be65da9ed267f17cc3d389d231b
ammarshabir552299 <ammarshabir529@gmail.com>
2026-08-25 11:16:22 +0500

    new

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello from Jenkins CI/CD!");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end("<h1>Hello from Jenkins CI/CD! version1</h1>");
+});
+
+server.listen(3001, () => {
+  console.log("Server running at http://localhost:3001");
+});
